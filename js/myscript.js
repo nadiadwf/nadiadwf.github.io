ae973021ca14954e892ff315908e7c27a8b53c51
yatdezohta <38444651+yatdezohta@users.noreply.github.com>
2019-12-06 15:46:10 +0700

    modal & extend jquery

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	$('.mobile-nav').click(function(){
 		nav.slideToggle();
 	});
-	
+
 	$(window).resize(function(){
 		if($(window).width() >= 680){
 			nav.show();
@@ -15,3 +15,57 @@ $(document).ready(function(){
 		}
 	});
 });
+
+$.fn.uploadGambar = function () { //extending jquery
+	this.change(function(event) {
+    	if(this.files.length > 0){
+    		console.log(this.files[0]);
+ 			var name = this.files[0].name;
+ 			var size = this.files[0].size;
+
+ 			var ukuran = "";
+ 			if (size >= 1000000 ) { //MB
+ 				size = Math.ceil(size/1000000);
+ 				ukuran = size + "MB";
+ 			}
+ 			else if (size >= 1000) { //KB
+ 				size = Math.ceil(size/1000);
+ 				ukuran = size + "KB";
+ 			}
+ 			else if(size < 1000){
+ 				ukuran = size + "Byte";
+ 			}
+
+ 			$("#file_info").html(`
+ 				<p>Nama File : `+name+`</p>
+ 				<p>Ukuran File : `+ukuran+`</p>
+ 				`
+ 			);
+
+    		if (this.files && this.files[0]) {
+    		  var reader = new FileReader();
+
+    		  reader.onload = function(e) {
+    		    $('#gambar_wraper').show();
+    		    $('#avatar_img').attr('src', e.target.result);
+    		  }
+
+    		  reader.readAsDataURL(this.files[0]);
+    		}
+    	}
+	});
+}
+//buka
+function modalShow() {
+	$("#id_modal").show();
+	$("#id_modal").css('animation-name', 'show_modal');
+	// $(".modal-content:first").css('animation-name', 'show_modal');
+}
+//nutup
+function modalHide() {
+	// $(".modal-content:first").css('animation-name', 'hide_modal');
+	$("#id_modal").css('animation-name', 'hide_modal');
+	setTimeout(function function_name() {
+		$("#id_modal").hide();
+	}, 300);
+}
